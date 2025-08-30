4a0c0bf8e47863df60fda94ebf12f7a94afd9ed1
Indrayani04 <rahateindrayani44@gmail.com>
2025-08-30 11:44:07 +0530

    Update test_greeter.py

--- test_greeter.py
+++ test_greeter.py
@@ -4,8 +4,8 @@ from greeter import Greeter
 class TestGreeter(unittest.TestCase):
     
     def test_say_hello(self):
-        g = Greeter("Siddharth")
-        self.assertEqual(g.say_hello(),"Hello, Gagan!")
+        g = Greeter("Indrayani")
+        self.assertEqual(g.say_hello(),"Hello,Indrayani")
 
 
 if __name__ == "__main__":
